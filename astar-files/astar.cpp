e2313793f72ed15ce02631fabdec0b49d5266871
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-02-07 06:20:53 -0800

    comment debug code

--- astar-files/astar.cpp
+++ astar-files/astar.cpp
@@ -3,6 +3,7 @@
 template <typename GraphType, typename Heuristic> 
 std::vector<typename GraphType::Edge> Astar<GraphType, Heuristic>::search(size_t s, size_t g) 
 {
+    //std::cout<<"start"<<std::endl;
     start_id = s;
     goal_id  = g;
     openlist.clear();
@@ -32,20 +33,25 @@ std::vector<typename GraphType::Edge> Astar<GraphType, Heuristic>::search(size_t
         {
             size_t t_heu = heuristic(graph, graph.GetVertex(t), goal_vertex);
             size_t current_heu = heuristic(graph, graph.GetVertex(current_id), goal_vertex);
-
-            if(f_score[t] > f_score[current_id] ||( f_score[t] == f_score[current_id] && t_heu < current_heu))
+            std::cout<<"current t id: "<<graph.GetVertex(t).ID()<<std::endl;
+            std::cout<<"current t f_score[t]: "<<f_score[t]<<std::endl;
+            if(f_score[t] < f_score[current_id] ||( f_score[t] == f_score[current_id] && t_heu < current_heu))
             {
                 current_id = t;
             }
         }
 
-        openlist.erase(openlist.begin());
+        auto it = openlist.find(current_id);
+        if (it != openlist.end()) 
+        {
+            openlist.erase(it);
+        }
         
-       // std::cout<<"current id"<<current_id<<"graph"<<graph.GetVertex(current_id).ID()<<std::endl;
+        std::cout<<"current id"<<current_id<<std::endl;
 
         if (current_id == goal_id) 
         {
-            //std::cout<<"goal_id"<<goal_id<<std::endl;
+            std::cout<<"goal_id"<<goal_id<<std::endl;
             size_t trace_id = goal_id;
             while (trace_id != start_id) 
             {
@@ -55,15 +61,13 @@ std::vector<typename GraphType::Edge> Astar<GraphType, Heuristic>::search(size_t
                 {
                     if (edge.GetID2() == trace_id) 
                     {
-                        //std::cout<<"edge.GetID()"<<edge.GetID1()<<std::endl;
+                        std::cout<<"edge.GetID()"<<edge.GetID1()<<std::endl;
                         solution.push_back(edge);
                         break;
                     }
                 }
                 trace_id = prev_id;
             }
-            std::reverse(solution.begin(), solution.end()); 
-
             callback.OnFinish(*this);
             return solution;
         }
@@ -73,18 +77,23 @@ std::vector<typename GraphType::Edge> Astar<GraphType, Heuristic>::search(size_t
         for (const Edge& neighbor : graph.GetOutEdges(current_id)) 
         {
             size_t neighbor_id = neighbor.GetID2();
-                
-            if (closedlist.find(neighbor_id) != closedlist.end())
+            std::cout<<"neighbor_id: "<<neighbor_id<<std::endl;    
+            /*if (closedlist.find(neighbor_id) != closedlist.end())
             {
                 continue;
-            }
+            }*/
 
             double tentative_g = g_score[current_id] + neighbor.GetWeight();
+            std::cout<<"tentative_g: "<<tentative_g<<std::endl;    
 
             if (g_score.find(neighbor_id) == g_score.end() || tentative_g < g_score[neighbor_id]) 
             {
+                std::cout<<"insert: "<<neighbor_id<<std::endl;
                 g_score[neighbor_id] = tentative_g;
                 f_score[neighbor_id] = tentative_g + heuristic(graph, graph.GetVertex(neighbor_id), goal_vertex);
+                std::cout<<" g_score[neighbor_id]"<< g_score[neighbor_id]<<std::endl;
+                std::cout<<" f_score[neighbor_id]"<< f_score[neighbor_id]<<std::endl;
+                std::cout<<" heuristic(graph, graph.GetVertex(neighbor_id), goal_vertex)"<< heuristic(graph, graph.GetVertex(neighbor_id), goal_vertex)<<std::endl;
                 came_from[neighbor_id] = current_id; 
                 openlist.insert(neighbor_id);
             }
@@ -92,7 +101,7 @@ std::vector<typename GraphType::Edge> Astar<GraphType, Heuristic>::search(size_t
 
         callback.OnIteration(*this);
     }
-
+    std::cout<<"end"<<std::endl;
     callback.OnFinish( *this );
     return solution;
 }
