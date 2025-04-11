556f590b4c800763915b90990a6611108902a5b3
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-04-11 05:34:13 -0700

    implement prove by refutation

--- pl-files/pl.cpp
+++ pl-files/pl.cpp
@@ -10,24 +10,95 @@ CNF const operator>( Literal const& op1, CNF     const& op2 ) { return CNF(op1)>
 
 KnowledgeBase::KnowledgeBase() : clauses() {}
 ////////////////////////////////////////////////////////////////////////////
-KnowledgeBase& KnowledgeBase::operator+=( CNF const& cnf ) {
-    for ( std::set< Clause >::const_iterator it = cnf.begin(); it != cnf.end(); ++it ) {
+KnowledgeBase& KnowledgeBase::operator+=( CNF const& cnf ) 
+{
+    for ( std::set< Clause >::const_iterator it = cnf.begin(); it != cnf.end(); ++it ) 
+    {
         clauses.insert( *it );
     }
+
     return *this;
 }
 ////////////////////////////////////////////////////////////////////////
-std::set< Clause >::const_iterator KnowledgeBase::begin() const { return clauses.begin(); }
-std::set< Clause >::const_iterator KnowledgeBase::end()   const { return clauses.end(); }
-unsigned                           KnowledgeBase::size()  const { return clauses.size(); }
-////////////////////////////////////////////////////////////////////////////
-bool KnowledgeBase::ProveByRefutation( CNF const& alpha ) const {
+std::set< Clause >::const_iterator KnowledgeBase::begin() const 
+{ 
+    return clauses.begin(); 
 }
+
+std::set< Clause >::const_iterator KnowledgeBase::end()   const 
+{ 
+    return clauses.end(); 
+}
+
+unsigned KnowledgeBase::size()  const 
+{ 
+    return clauses.size(); 
+}
+
 ////////////////////////////////////////////////////////////////////////////
-std::ostream& operator<<( std::ostream& os, KnowledgeBase const& kb ) {
+bool KnowledgeBase::ProveByRefutation( CNF const& alpha ) const 
+{
+    std::set<Clause> kbClauses = this->clauses;       
+    CNF negatedAlpha = ~alpha;
+
+    for (const Clause& c : negatedAlpha)
+    {
+        kbClauses.insert(c);
+    }
+
+    std::set<Clause> newClauses;
+
+    bool addedNewClause = true;
+    while (addedNewClause) 
+    {
+        addedNewClause = false;
+        std::vector<Clause> clauseVec(kbClauses.begin(), kbClauses.end());
+
+        // Resolve all pairs
+        for (size_t i = 0; i < clauseVec.size(); ++i) 
+        {
+            for (size_t j = i + 1; j < clauseVec.size(); ++j) 
+            {
+                const Clause& c1 = clauseVec[i];
+                const Clause& c2 = clauseVec[j];
+
+                std::set<Clause> resolvents = c1.Resolve(c2); 
+
+                for (const Clause& r : resolvents) 
+                {
+                    // Contradiction
+                    if (r.size() == 0) 
+                    {
+                        return true;
+                    }
+
+                    if (kbClauses.find(r) == kbClauses.end() && newClauses.find(r) == newClauses.end()) 
+                    {
+                        newClauses.insert(r);
+                        addedNewClause = true;
+                    }
+                }
+            }
+        }
+
+        // Merge new clauses into the KB
+        kbClauses.insert(newClauses.begin(), newClauses.end());
+        newClauses.clear();
+    }
+
+    // No contradiction
+    return false;
+}
+
+////////////////////////////////////////////////////////////////////////////
+std::ostream& operator<<( std::ostream& os, KnowledgeBase const& kb ) 
+{
     unsigned size = kb.clauses.size();
-    for( std::set< Clause >::const_iterator it1 = kb.clauses.begin(); it1 != kb.clauses.end(); ++it1) { 
+
+    for( std::set< Clause >::const_iterator it1 = kb.clauses.begin(); it1 != kb.clauses.end(); ++it1) 
+    { 
         os << *it1 << ", ";
     }
+
     return os;
 }
--- pl-files/pl.h
+++ pl-files/pl.h
@@ -11,18 +11,38 @@
 
 class Literal {
     public:
-        Literal( std::string const& _name ) : name(_name), negated(false) { }
-        Literal( ) : name(""), negated(false) { } // just for map.operator[]
+        Literal( std::string const& _name ) : name(_name), negated(false) 
+        {
+
+        }
+
+        Literal( ) : name(""), negated(false) 
+        {
+            
+        }
+         // just for map.operator[]
         ////////////////////////////////////////////////////////////////////////
-        Literal& Negate() { negated=!negated; }
-        bool IsPositive() const { return !negated; }
+        Literal& Negate() 
+        {
+            negated=!negated; 
+            return *this;
+        }
+
+        bool IsPositive() const 
+        { 
+            return !negated; 
+        }
         ////////////////////////////////////////////////////////////////////////
-        bool operator==( Literal const& op2 ) const {
+
+        bool operator==( Literal const& op2 ) const 
+        {
             Literal const& op1 = *this;
             return ( op1.negated == op2.negated ) && ( op1.name == op2.name );
         }
+
         ////////////////////////////////////////////////////////////////////////
-        bool operator<( Literal const& op2 ) const {
+        bool operator<( Literal const& op2 ) const 
+        {
             Literal const& op1 = *this;
             //negated infront
             if ( op1.negated && !op2.negated ) {
@@ -34,17 +54,22 @@ class Literal {
             return ( op1.name < op2.name );
         }
         ////////////////////////////////////////////////////////////////////////
-        Literal operator~() const { 
+
+        Literal operator~() const 
+        { 
             Literal result( *this );
             result.Negate();
             return result;
         }
+
         ////////////////////////////////////////////////////////////////////////
-        bool Complementary( Literal const& op2 ) const {
+        bool Complementary( Literal const& op2 ) const
+        {
             return ( name == op2.name ) && ( negated != op2.negated );
         }
         ////////////////////////////////////////////////////////////////////////
-        friend std::ostream& operator<<( std::ostream& os, Literal const& literal ) {
+        friend std::ostream& operator<<( std::ostream& os, Literal const& literal ) 
+        {
             os << (literal.negated?"~":"") << literal.name;
             return os;
         }
@@ -57,8 +82,10 @@ class Clause {
     public:
         Clause() = default;
 
-        Clause(std::initializer_list<Literal> init) {
-            for (auto const& lit : init) {
+        Clause(std::initializer_list<Literal> init) 
+        {
+            for (auto const& lit : init) 
+            {
                 literals.insert(lit);
             }
         }
@@ -73,26 +100,83 @@ class Clause {
             literals.insert(lit);
         }
 
-        std::set<Literal>::const_iterator begin() const { return literals.begin(); }
-        std::set<Literal>::const_iterator end() const { return literals.end(); }
+        std::set<Clause> Resolve(const Clause& other) const 
+        {
+            std::set<Clause> resolvents;
+        
+            for (const Literal& lit : literals) 
+            {
+                //negate the literal
+                Literal negated = ~lit; 
+        
+                if (other.literals.find(negated) != other.literals.end()) 
+                {
+                    //found a complementary literal
+                    Clause resolvent;
+        
+                    // Add all literals from both clauses except the complementary pair
+                    for (const Literal& l : literals)
+                    {
+                        if (!(l == lit))
+                        {
+                            resolvent.literals.insert(l);
+                        }
+                    }
 
-        Clause const operator|(Clause const& other) const {
+        
+                    for (const Literal& l : other.literals)
+                    {
+                        if (!(l == negated))
+                        {
+                            resolvent.literals.insert(l);
+                        }
+                    }
+
+                    resolvents.insert(resolvent);
+                }
+            }
+        
+            return resolvents;
+        }
+
+        std::set<Literal>::const_iterator begin() const 
+        { 
+            return literals.begin(); 
+        }
+
+        std::set<Literal>::const_iterator end() const 
+        { 
+            return literals.end(); 
+        }
+
+        bool operator<(Clause const& other) const 
+        {
+            return literals < other.literals;
+        }
+
+        Clause const operator|(Clause const& other) const 
+        {
             Clause result = *this;
             result.literals.insert(other.literals.begin(), other.literals.end());
             return result;
         }
 
         ////////////////////////////////////////////////////////////////////////
-        friend std::ostream& operator<<( std::ostream& os, Clause const& clause ) {
+        friend std::ostream& operator<<( std::ostream& os, Clause const& clause ) 
+        {
             unsigned size = clause.literals.size();
 
-            if ( clause.size() == 0 ) {
+            if ( clause.size() == 0 ) 
+            {
                 os << " FALSE ";
-            } else {
+            } 
+            else 
+            {
                 std::set< Literal >::const_iterator it = clause.literals.begin();
                 os << "( " << *it;
                 ++it;
-                for ( ; it!=clause.literals.end(); ++it ) {
+                for ( ; it!=clause.literals.end(); ++it ) 
+                {
                     os << " | " << *it;
                 }
                 os << " ) ";
@@ -107,7 +191,8 @@ class CNF {
     public:
         CNF() = default;
 
-        CNF(Literal const& lit) {
+        CNF(Literal const& lit) 
+        {
             Clause clause;
             clause.AddLiteral(lit);      
             clauses.insert(clause);     
@@ -117,8 +202,6 @@ class CNF {
             clauses.insert(clause);
         }
     
-        // ..........
-        // ..........
         ////////////////////////////////////////////////////////////////////////
         // not
         CNF const operator~( ) const {
@@ -133,7 +216,9 @@ class CNF {
             if (clauses.size() == 1) 
             {
                 const Clause& clause = *clauses.begin();
-                for (const Literal& lit : clause) {
+
+                for (const Literal& lit : clause) 
+                {
                     Literal negatedLit = ~lit;
                     Clause newClause;
                     newClause.AddLiteral(negatedLit);
@@ -143,9 +228,11 @@ class CNF {
             else
             {
                 std::vector<CNF> negatedClauses;
-                for (const Clause& clause : clauses) {
+                for (const Clause& clause : clauses) 
+                {
                     CNF temp;
-                    for (const Literal& lit : clause) {
+                    for (const Literal& lit : clause) 
+                    {
                         Clause singleClause;
                         singleClause.AddLiteral(~lit);
                         temp = temp & CNF(singleClause); // ~clause = ~A & ~B & ~C
@@ -154,7 +241,8 @@ class CNF {
                 }
 
                 result = negatedClauses[0];
-                for (size_t i = 1; i < negatedClauses.size(); ++i) {
+                for (size_t i = 1; i < negatedClauses.size(); ++i) 
+                {
                     result = result | negatedClauses[i];
                 }
             }
@@ -170,16 +258,14 @@ class CNF {
         }
         ////////////////////////////////////////////////////////////////////////
         // and
-        CNF const operator&( CNF const& op2 ) const {
+        CNF const operator&( CNF const& op2 ) const 
+        {
             //CNF1 = clause1 & clause2 & clause3,
             //CNF2 = clause4 & clause5 & clause6,
             //CNF1 & CNF2 = clause1 & clause2 & clause3 & clause4 & clause5 & clause6
             CNF result;
-            
             result.clauses.insert(this->clauses.begin(), this->clauses.end());
-
             result.clauses.insert(op2.clauses.begin(), op2.clauses.end());
-
             return result;
         }
         ///////////////////////////////////////////////////////////////////////
@@ -191,11 +277,12 @@ class CNF {
             //              c1|c4 & c1|c5 & c1|c6    &
             //              c2|c4 & c2|c5 & c2|c6    &
             //              c3|c4 & c3|c5 & c3|c6
-
             CNF result;
 
-            for (const Clause& c1 : this->clauses) {
-                for (const Clause& c2 : op2.clauses) {
+            for (const Clause& c1 : this->clauses) 
+            {
+                for (const Clause& c2 : op2.clauses) 
+                {
                     Clause combined = c1 | c2;
                     result.clauses.insert(combined);
                 }
@@ -205,9 +292,20 @@ class CNF {
         }
 
         /////////////////////////////////////////////////////////////////////////////////
-        CNF const operator>( Literal const& op2 ) const { return operator>( CNF(op2) ); }
-        CNF const operator&( Literal const& op2 ) const { return operator&( CNF(op2) ); }
-        CNF const operator|( Literal const& op2 ) const { return operator|( CNF(op2) ); }
+        CNF const operator>( Literal const& op2 ) const 
+        { 
+            return operator>( CNF(op2) ); 
+        }
+
+        CNF const operator&( Literal const& op2 ) const 
+        { 
+            return operator&( CNF(op2) ); 
+        }
+
+        CNF const operator|( Literal const& op2 ) const 
+        { 
+            return operator|( CNF(op2) ); 
+        }
 
         ////////////////////////////////////////////////////////////////////////
         bool Empty() const 
@@ -219,9 +317,11 @@ class CNF {
         std::set< Clause >::const_iterator end()   const { return clauses.end(); }
         unsigned                           size()  const { return clauses.size(); }
         ////////////////////////////////////////////////////////////////////////
-        friend std::ostream& operator<<( std::ostream& os, CNF const& cnf ) {
+        friend std::ostream& operator<<( std::ostream& os, CNF const& cnf ) 
+        {
             unsigned size = cnf.clauses.size();
-            for( std::set< Clause >::const_iterator it1 = cnf.clauses.begin(); it1 != cnf.clauses.end(); ++it1) { 
+            for( std::set< Clause >::const_iterator it1 = cnf.clauses.begin(); it1 != cnf.clauses.end(); ++it1) 
+            { 
                 os << *it1 << ", ";
             }
             return os;
@@ -240,7 +340,8 @@ CNF const operator>( Literal const& op1, CNF     const& op2 );
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-class KnowledgeBase {
+class KnowledgeBase 
+{
     public:
         ////////////////////////////////////////////////////////////////////////////
         KnowledgeBase();
